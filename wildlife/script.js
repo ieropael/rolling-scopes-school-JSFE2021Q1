c0361dd70c2ac6c84031143badf7497253472f13
Aleksandr Petrov <ieropael@gmail.com>
2021-03-12 19:34:44 +0400

    feat: add responsiveness and better pixel perfect

--- wildlife/script.js
+++ wildlife/script.js
@@ -3,17 +3,29 @@ let arrowLeft = document.querySelector(".arrow-left");
 let slides = document.querySelectorAll(".articles__slide");
 let i = 0;
 arrowRight.addEventListener("click", function () {
-  if (i < 3) {
+  if (i < slides.length - 3) {
     slides[i].classList.remove("visible");
     slides[i + 3].classList.add("visible");
     i++;
   } else {
-    i = 3;
+    slides[i].classList.remove("visible");
+    slides[i + 1].classList.remove("visible");
+    slides[i + 2].classList.remove("visible");
+    slides[1].classList.add("visible");
+    slides[2].classList.add("visible");
+    slides[3].classList.add("visible");
+    i = 1;
   }
 })
 arrowLeft.addEventListener("click", function () {
-  if (i < 1) {
-    i = 0;
+  if (i == 0) {
+    slides[i].classList.remove("visible");
+    slides[i + 1].classList.remove("visible");
+    slides[i + 2].classList.remove("visible");
+    slides[slides.length - 4].classList.add("visible");
+    slides[slides.length - 3].classList.add("visible");
+    slides[slides.length - 2].classList.add("visible");
+    i = 5;
   } else {
     slides[i - 1].classList.add("visible");
     slides[i + 2].classList.remove("visible");
