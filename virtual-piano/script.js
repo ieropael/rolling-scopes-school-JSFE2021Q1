a16f2a34dbf1f50e44f6c45a61842e7d96f0d5f3
Aleksandr Petrov <ieropael@gmail.com>
2021-03-21 23:13:02 +0400

    feat: add note playback on mouse hover

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,6 +1,36 @@
 const piano = document.querySelector('.piano');
 const pianoKeys = document.querySelectorAll('.piano-key');
 
+function startCorrespondOver(event) {
+  startSound(event);
+  pianoKeys.forEach((elem) => {
+    elem.addEventListener('mouseover', startSound);
+    elem.addEventListener('mouseout', stopSound);
+  });
+}
+
+function stopCorrespondOver() {
+  pianoKeys.forEach((elem) => {
+    elem.removeEventListener('mouseover', startSound);
+    elem.classList.remove('piano-key-active');
+  });
+}
+
+function startSound(event) {
+  if (event.target.classList.contains('piano-key')) {
+    event.target.classList.add('piano-key-active');
+    const note = event.target.dataset.note;
+    const src = `assets/audio/${note}.mp3`;
+    playAudio(src);
+  }
+}
+
+function stopSound(event) {
+  if (event.target.classList.contains('piano-key')) {
+    event.target.classList.remove('piano-key-active');
+  }
+}
+
 function playAudio(src) {
   const audio = new Audio();
   audio.src = src;
@@ -8,24 +38,8 @@ function playAudio(src) {
   audio.play();
 }
 
-piano.addEventListener('mousedown', (event) => {
-  if (event.target.classList.contains('piano-key')) {
-    const note = event.target.dataset.note;
-    const src = `assets/audio/${note}.mp3`;
-    playAudio(src);
-  }
-});
-
-piano.addEventListener('mousedown', (event) => {
-  if (event.target.classList.contains('piano-key')) {
-    pianoKeys.forEach((elem) => {
-      if (elem.classList.contains('piano-key-active')) {
-        elem.classList.remove('piano-key-active');
-      }
-    });
-    event.target.classList.add('piano-key-active');
-  }
-});
+piano.addEventListener('mousedown', startCorrespondOver);
+document.addEventListener('mouseup', stopCorrespondOver);
 
 window.addEventListener('keydown', (event) => {
   pianoKeys.forEach((elem) => {
