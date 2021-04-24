a900ed62e184dfb7b3790e8ee6a5610653b7b7b1
Aleksandr Petrov <ieropael@gmail.com>
2021-04-24 21:38:39 +0400

    feat: implement reset function by clicking "Reset" button

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,4 +1,5 @@
 const inputs = document.querySelectorAll('.filters input');
+const resetButton = document.querySelector('.btn-reset');
 
 function handleUpdate() {
   const suffix = this.dataset.sizing || '';
@@ -6,4 +7,18 @@ function handleUpdate() {
   this.nextElementSibling.value = this.value;
 }
 
+function reset() {
+  inputs.forEach(input => {
+    const suffix = input.dataset.sizing || '';
+    input.value = input.min;
+    input.nextElementSibling.value = input.min;
+    if (input.name === 'saturate') {
+      input.value = 100;
+      input.nextElementSibling.value = 100;
+    }
+    document.documentElement.style.setProperty(`--${input.name}`, input.value + suffix);
+  });
+}
+
 inputs.forEach(input => input.addEventListener('input', handleUpdate));
+resetButton.addEventListener('click', reset);
