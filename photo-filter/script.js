b5eb953cef5a3308c8ab70d008b6bf14502b29b6
Aleksandr Petrov <ieropael@gmail.com>
2021-04-25 00:10:31 +0400

    fix: fix pictures changing

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -3,7 +3,7 @@ const resetButton = document.querySelector('.btn-reset');
 const nextButton = document.querySelector('.btn-next');
 const image = document.querySelector('img');
 const base = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/night/';
-const images = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
+const images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
 let i = 0;
 
 function handleUpdate() {
