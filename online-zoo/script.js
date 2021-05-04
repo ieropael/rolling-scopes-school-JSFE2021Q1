c66c59767eb1202cb1742279e7e511ef76f31a10
Aleksandr Petrov <ieropael@gmail.com>
2021-05-04 19:12:13 +0400

    fix: make correct order of flipping pictures

--- online-zoo/script.js
+++ online-zoo/script.js
@@ -12,27 +12,30 @@ function update(newActive) {
   const next = elems.find((elem) => elem.dataset.pos == 1);
   const first = elems.find((elem) => elem.dataset.pos == -2);
   const last = elems.find((elem) => elem.dataset.pos == 2);
-  
+
   [current, prev, next, first, last].forEach(item => {
     let itemPos = item.dataset.pos;
-    item.dataset.pos = getPos(itemPos, newActivePos)
+    item.dataset.pos = getPos(itemPos, newActivePos);
   });
 };
 
 function getPos(current, active) {
   const diff = current - active;
 
-  if (Math.abs(current - active) > 2) {
-    return -current
+  if (Math.abs(diff) > 2) {
+    return diff > 0 ? diff - carouselItems.length : diff + carouselItems.length;
   }
 
   return diff;
 }
 
 function startCarousel(event) {
-  update(event.target.parentNode);
-  
+  if (event.target.classList.contains('how-it-works__picture')) {
+    update(event.target.parentNode);
+  }
+
   carouselList.removeEventListener('click', startCarousel);
+  
   setTimeout(function() {
     carouselList.addEventListener('click', startCarousel);
   }, 500);
