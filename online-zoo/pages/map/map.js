3f2e165c0f8d927e20ae122506c1f09d28409ef8
Aleksandr Petrov <ieropael@gmail.com>
2021-05-06 16:54:27 +0400

    fix: map doesn't go behind wrapper during drag & drop

--- online-zoo/pages/map/map.js
+++ online-zoo/pages/map/map.js
@@ -1,6 +1,7 @@
 const zoomPlus = document.querySelector('.zoom__plus');
 const zoomMinus = document.querySelector('.zoom__minus');
 const map = document.querySelector('.map');
+const mapWrapper = document.querySelector('.map__wrapper')
 
 function zoomIn() {
   let currWidth = map.clientWidth;
@@ -21,13 +22,31 @@ function zoomOut() {
   if (currWidth > wrapperWidth) {
     map.style.width = (currWidth - 100) + 'px';
     map.style.height = (currHeigth - (currWidth / currHeigth) * 100 + 'px');
+    if (map.clientHeight + parseInt(map.style.top) < mapWrapper.clientHeight) {
+      map.style.top = mapWrapper.clientHeight - map.clientHeight + 'px';
+    }
+    if (map.clientWidth + parseInt(map.style.left) < mapWrapper.clientWidth) {
+      map.style.left = mapWrapper.clientWidth - map.clientWidth + 'px';
+    }
+    if (map.clientWidth == mapWrapper.clientWidth) {
+      map.style.top = (mapWrapper.clientHeight - map.clientHeight) / 2 + 'px';
+    }
   } else {
     return false;
   }
 }
 
-zoomPlus.addEventListener('click', () => zoomIn());
-zoomMinus.addEventListener('click', () => zoomOut());
+zoomPlus.addEventListener('click', zoomIn);
+zoomMinus.addEventListener('click', zoomOut);
+
+function moveAt(pageX, pageY, shiftX, shiftY) {
+  if (pageX - shiftX <= 0 && pageX - shiftX >= mapWrapper.offsetWidth - map.offsetWidth) {
+    map.style.left = pageX - shiftX + 'px';
+  }
+  if (pageY - shiftY <= -80 && pageY - shiftY >= mapWrapper.offsetHeight - map.offsetHeight) {
+    map.style.top = pageY - shiftY + 'px';
+  }
+}
 
 map.onmousedown = function(event) {
   let windowWidth = document.documentElement.clientWidth;
@@ -39,15 +58,10 @@ map.onmousedown = function(event) {
   let shiftX = event.clientX - map.getBoundingClientRect().left;
   let shiftY = event.clientY - map.getBoundingClientRect().top;
 
-  moveAt(event.pageX - fixWidth, event.pageY - 180);
-
-  function moveAt(pageX, pageY) {
-    map.style.left = pageX - shiftX + 'px';
-    map.style.top = pageY - shiftY + 'px';
-  }
+  moveAt(event.pageX - fixWidth, event.pageY - 180, shiftX, shiftY);
 
   function onMouseMove(event) {
-    moveAt(event.pageX - fixWidth, event.pageY - 180);
+    moveAt(event.pageX - fixWidth, event.pageY - 180, shiftX, shiftY);
   }
 
   document.addEventListener('mousemove', onMouseMove);
