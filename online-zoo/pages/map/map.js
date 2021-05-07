de9e59e23068fc7898111c57169a05c455b426e2
Aleksandr Petrov <ieropael@gmail.com>
2021-05-07 11:16:05 +0400

    feat: add modal windows to all pages

--- online-zoo/pages/map/map.js
+++ online-zoo/pages/map/map.js
@@ -81,3 +81,95 @@ map.onmousedown = function(event) {
 map.ondragstart = function() {
   return false;
 };
+
+// MODAL WINDOW
+
+const donateButton = document.querySelector('.footer__button');
+
+const modal = document.querySelector('.modal');
+const modalPay = document.querySelector('.modal__pay');
+const modalContent = document.querySelector('.modal__content');
+
+const closeModal = document.querySelectorAll('.modal__close');
+const nextButton = document.querySelector('.modal__button');
+const payButton = document.querySelector('.modal__pay-button');
+
+const inputFields = document.querySelectorAll('.input-field');
+const inputArray = Array.from(inputFields);
+
+function maxLengthCheck(field) {
+  if (field.type == 'number' && field.value.length >= field.max.length) {
+    field.value = field.value.slice(0, field.max.length);
+    field.style.border = "1px solid #9ca498";
+  }
+}
+
+inputFields.forEach(field => field.addEventListener('input', event => maxLengthCheck(event.target)));
+  
+function isNumeric(event) {
+  let theEvent = event || window.event;
+  let key = theEvent.keyCode || theEvent.which;
+  key = String.fromCharCode(key);
+  let regex = /[0-9]|\./;
+  if (!regex.test(key)) {
+    theEvent.returnValue = false;
+    if (theEvent.preventDefault) {
+      theEvent.preventDefault()
+    };
+  }
+}
+
+function validateInput(field) {
+  if (field.value.length < field.max.length || field.value == null || field.value == '') {
+    field.style.border = "1px solid red";
+    return false;
+  } else {
+    field.style.border = "1px solid #9ca498";
+    return true;
+  }
+}
+
+donateButton.addEventListener('click', () => {
+  modal.style.display = 'flex';
+  modalContent.style.display = 'flex';
+  modalPay.style.display = 'none';
+});
+
+closeModal.forEach(close => close.addEventListener('click', () => {
+  modal.style.display = "none";
+}));
+
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
+
+nextButton.addEventListener('click', () => {
+  let checkInput = true;
+  if (!validateInput(inputArray[0])) {
+    checkInput = false;
+  }
+  if (checkInput) {
+    modalContent.style.display = 'none';
+    modalPay.style.display = 'flex';
+  } else {
+    alert('Please fill in all required fields!');
+  }
+});
+
+payButton.addEventListener('click', () => {
+  let checkInputs = true;
+  for (let i = 1; i < inputArray.length; i++) {
+    if (!validateInput(inputArray[i])) {
+      checkInputs = false;
+    }
+  }
+  if (checkInputs) {
+    modal.style.display = 'none';
+    alert('Thank you for your donation!');
+    inputFields.forEach(input => input.value = '');
+  } else {
+    alert('Please fill in all required fields!');
+  }
+});
