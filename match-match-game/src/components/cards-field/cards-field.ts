670a4f97ba1364c77098d33fb29e957355c41f6d
Aleksandr Petrov <ieropael@gmail.com>
2021-05-28 21:10:25 +0400

    feat: add methods to 'cards-field' component

--- match-match-game/src/components/cards-field/cards-field.ts
+++ match-match-game/src/components/cards-field/cards-field.ts
@@ -1,8 +1,21 @@
 import './cards-field.css';
 import { BaseComponent } from '../base-component';
+import { Card } from '../card/card';
 
 export class CardsField extends BaseComponent {
+  private cards: Card[] = [];
+
   constructor() {
     super('div', ['cards-field']);
+  }
+
+  clear() {
+    this.cards = [];
+    this.element.innerHTML = '';
+  }
+
+  addCards(cards: Card[]) {
+    this.cards = cards;
+    this.cards.forEach((card) => this.element.appendChild(card.element));
   }
 }
