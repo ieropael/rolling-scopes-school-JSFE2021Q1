5a531bbd98869458827268d95d9614edc48ddf88
Aleksandr Petrov <ieropael@gmail.com>
2021-05-28 13:03:02 +0400

    feat: add 'app' class

--- match-match-game/src/index.ts
+++ match-match-game/src/index.ts
@@ -1,1 +1,9 @@
 import './style.css';
+import { App } from './app';
+
+window.onload = () => {
+  const appElement = document.getElementById('app');
+
+  if (!appElement) throw Error('App root element not found');
+  return new App(appElement);
+};
