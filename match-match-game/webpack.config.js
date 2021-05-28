5ec7be45dbf9a69dd630e5ae61d65926b3121235
Aleksandr Petrov <ieropael@gmail.com>
2021-05-28 22:15:48 +0400

    feat: add 'copy-webpack-plugin'

--- match-match-game/webpack.config.js
+++ match-match-game/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
@@ -54,6 +55,11 @@ module.exports = ({develop}) => ({
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {from: './public'}
+      ]
     }),
     new CleanWebpackPlugin(),
     ...esLintPlugin(develop),
