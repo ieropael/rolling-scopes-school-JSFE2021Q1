422ec5755f7bf2c651524d4395e002b4f19566e2
Aleksandr Petrov <ieropael@gmail.com>
2021-05-28 21:46:43 +0400

    feat: add card handler method in 'game' component

--- match-match-game/src/components/game/game.ts
+++ match-match-game/src/components/game/game.ts
@@ -1,9 +1,16 @@
 import { BaseComponent } from '../base-component';
 import { Card } from '../card/card';
 import { CardsField } from '../cards-field/cards-field';
+import { delay } from '../shared/delay';
+
+const FLIP_DELAY = 3000;
 
 export class Game extends BaseComponent {
   private readonly cardsField: CardsField;
+
+  private activeCard?: Card;
+
+  private isAnimation = false;
 
   constructor() {
     super();
@@ -11,11 +18,34 @@ export class Game extends BaseComponent {
     this.element.appendChild(this.cardsField.element);
   }
 
-  initGame(images: string[]) {
+  initGame(images: string[]): void {
     this.cardsField.clear();
 
     const cards: Card[] = images.concat(images).map((url: string) => new Card(url)).sort(() => Math.random() - 0.5);
 
+    cards.forEach((card) => card.element.addEventListener('click', () => this.cardHandler(card)));
+
     this.cardsField.addCards(cards);
+  }
+
+  private async cardHandler(card: Card) {
+    if (this.isAnimation) {
+      return;
+    }
+    this.isAnimation = true;
+    await card.flipToFront();
+
+    if (!this.activeCard) {
+      this.activeCard = card;
+      return;
+    }
+
+    if (this.activeCard.image !== card.image) {
+      await delay(FLIP_DELAY);
+      await Promise.all([this.activeCard.flipToBack(), card.flipToBack()]);
+    }
+
+    this.activeCard = undefined;
+    this.isAnimation = false;
   }
 }
