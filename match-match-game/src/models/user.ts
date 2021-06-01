351148228e600e27c71de60564dcba56d45855c4
Aleksandr Petrov <ieropael@gmail.com>
2021-06-01 19:38:52 +0400

    feat: add 'user' class

---
+++ match-match-game/src/models/user.ts
@@ -0,0 +1,13 @@
+export class User {
+  firstname?: string;
+
+  lastname?: string;
+
+  email?: string;
+
+  constructor(firstname: string, lastname: string, email: string) {
+    this.firstname = firstname,
+    this.lastname = lastname,
+    this.email = email;
+  }
+}
