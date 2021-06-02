9efa3bea3320a761dd0eb1d8da862d472a6317cd
Aleksandr Petrov <ieropael@gmail.com>
2021-06-02 13:04:57 +0400

    refactor: remove unnecessary code

--- match-match-game/src/index.ts
+++ match-match-game/src/index.ts
@@ -1,12 +1,5 @@
 import './style.css';
 import App from './app';
 
-// window.onload = () => {
-//   const appElement = document.getElementById('app');
-
-//   if (!appElement) throw Error('App root element not found');
-//   new App(appElement).start();
-// };
-
 const app = new App();
 app.run();
