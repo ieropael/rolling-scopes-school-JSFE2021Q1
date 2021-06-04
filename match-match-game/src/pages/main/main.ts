4107e442c3191a44d976344730edfcb463cdbf15
Aleksandr Petrov <ieropael@gmail.com>
2021-06-04 19:50:58 +0400

    fix: fix 'about us' page styles

--- match-match-game/src/pages/main/main.ts
+++ match-match-game/src/pages/main/main.ts
@@ -6,37 +6,35 @@ import Page from '../../templates/page';
 
 export default class MainPage extends Page {
   static textObject = {
-    MainContent: `<h3>Уважаемый проверяющий! Приложение выглядит не очень, но весь основной функционал
-    работает. Проверьте, пожалуйста, не обращая внимания на этот ужас. :) Заранее, большое спасибо! Контакты для связи: 
-    Discord: ieropael#1569, Telegram: @ieropael</h3>
+    MainContent: `
     <div class="about-game">
-    <div class="about-game__content">
-      <h2 class="how-to-play">How to play?</h2>
-      <div class="register">
-        <div class="register__info">
-          <div class="register__number">1</div>
-          <div class="register__text">Register new player in game</div>
+      <div class="about-game__content">
+        <h2 class="how-to-play">How to play?</h2>
+        <div class="register">
+          <div class="register__info">
+            <div class="register__number">1</div>
+            <span class="register__text">Register new player in game</span>
+          </div>
+          <img class="register__example" src="../../assets/register.jpg">
         </div>
-        <img class="register__example" src="../../assets/register.jpg">
-      </div>
-      <div class="configure">
-        <div class="configure__info">
-          <div class="register__number">2</div>
-          <div class="register__text">Configure your game settings</div>
+        <div class="configure">
+          <div class="configure__info">
+            <div class="register__number">2</div>
+            <span class="register__text">Configure your game settings</span>
+          </div>
+          <img class="configure__example" src="../../assets/settings.jpg">
         </div>
-        <img class="configure__example" src="../../assets/settings.jpg">
-      </div>
-      <div class="start">
-        <div class="start__info">
-          <div class="register__number">3</div>
-          <div class="register__text">Start you new game! Remember card<br>
-          positions and match it before times up.</div>
+        <div class="start">
+          <div class="start__info">
+            <div class="register__number">3</div>
+            <span class="register__text">Start you new game! Remember card<br>
+            positions and match it before times up.</span>
+          </div>
+          <img class="start__example" src="../../assets/game.jpg">
         </div>
-        <img class="start__example" src="../../assets/game.jpg">
       </div>
     </div>
-  </div>
-  `,
+    `,
   };
 
   render(): HTMLElement {
